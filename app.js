12065a2e6b8529881aa823a9872e899d9263b84a
VaibhavB12 <vaibha.nikalje@gmail.com>
2024-08-27 17:03:10 +0530

    Part 2 globalErrorHandler middleware added 114 Better Errors and Refactoring

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 
 const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -33,14 +34,6 @@ app.all('*', (req, res, next)=>{
   next(new AppError(`Can't find ${req.originalUrl} on the server`, 404));
 });
 
-app.use((err, req, res, next)=>{
-  console.log(err.stack);
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
-});
+app.use(globalErrorHandler); // function moved to errorController.js
 
 module.exports = app;
