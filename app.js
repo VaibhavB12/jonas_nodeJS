fb4f2ef9e0b08a6e2a0e62390fe75c800c19e3a5
VaibhavB12 <vaibha.nikalje@gmail.com>
2024-08-27 15:06:43 +0530

    111 Handling Unhandled Routes

--- app.js
+++ app.js
@@ -28,23 +28,12 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+
 app.all('*', (req, res, next) => {
-  // res.status(404).json({
-  //   status: 'fail',
-  //   message: `Cant find ${req.originalUrl} on this server!` 
-  // });
-  const err = new Error(`Cant find ${req.originalUrl} on this server!`);
-  err.status = 'fail';
-  err.statusCode = 404;
-  next(err);
+  res.status(404).json({
+    status: 'fail',
+    message: `Cant find ${req.originalUrl} on this server!` 
+  });
 });
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500; // internal server error
-  err.status = err.status || 'error'
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  })
-})
 module.exports = app;
