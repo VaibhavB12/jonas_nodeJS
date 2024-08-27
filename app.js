f3dfdf39b2055ebe458c99193839b829ca0c16d1
VaibhavB12 <vaibha.nikalje@gmail.com>
2024-08-27 16:37:57 +0530

    Part 1 Check Stack trace in console Not created global error handling middleware 114 Better Errors and Refactoring

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -29,13 +30,11 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next)=>{
-  const err = new Error(`Can't find ${req.originalUrl} on this server`);
-  err.status = 'fail';
-  err.statusCode = 404;
-  next(err);
+  next(new AppError(`Can't find ${req.originalUrl} on the server`, 404));
 });
 
 app.use((err, req, res, next)=>{
+  console.log(err.stack);
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
   res.status(err.statusCode).json({
